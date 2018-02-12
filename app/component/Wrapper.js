3d03328c1be30fd99e09784117ddbad5fda7c389
freshzxf <370234987@qq.com>
2018-02-12 18:56:44 +0800

    添加注释

--- app/component/Wrapper.js
+++ app/component/Wrapper.js
@@ -10,7 +10,10 @@ import TabView from './TabView'
 
 export default class Wrapper extends Component {
     constructor(props) {
-        /* 继承构造函数的所有属性（如：this.props.navigator属性）*/
+        /* 继承构造函数的所有属性（如：this.props.navigator属性）
+        * super()调用会生成一个空对象，作为context来调用父类的constructor，返回this对象，作为子类constructor的context继续调用构造函数。
+        * 参考：https://www.cnblogs.com/liutie1030/p/5997446.html
+        * */
         super(props)
     }
 
